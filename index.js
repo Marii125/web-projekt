e868390a62f21f00d6dc1bfb6a8f31714a724ed3
Martina Bujasová <martina.bujasova2@gmail.com>
2024-05-18 17:48:38 +0200

    Update

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const mouseEl = document.querySelector('.mouse');
 const root = document.documentElement;
 
 document.addEventListener('mousemove', (e) => {
-  let x = -e.clientX / 2;
-  let y = -e.clientY / 2;
-  mouseEl.style.setProperty('--x', x + 'px');
-  mouseEl.style.setProperty('--y', y + 'px');
+  if (mouseEl !== null) {
+    let x = -e.clientX / 2;
+    let y = -e.clientY / 2;
+    mouseEl.style.setProperty('--x', x + 'px');
+    mouseEl.style.setProperty('--y', y + 'px');
+  }
 });
